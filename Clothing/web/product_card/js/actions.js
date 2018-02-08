4b72caeb0cd38ec64869bbdcec40255ee9f64395
laurinie <niemi.late@gmail.com>
2018-02-08 09:47:08 +0200

    smooth scroll to product card

--- Clothing/web/product_card/js/actions.js
+++ Clothing/web/product_card/js/actions.js
@@ -1,23 +1,50 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
+
+/*-----This opens and scrolls to productcard------*/
     let addProductBtn = document.querySelector("#add-button");
     let productCard = document.querySelector("#add-product");
-    addProductBtn.addEventListener('click', function () {
+    tn.addEventListener('click', function () {
 
         if (productCard.classList.contains("display-none")) {
             productCard.classList.remove("display-none");
-            }
-        
+        }
+        scrollSmooth("#add-product")
     });
 
+
+/*-----Close button for productcard------*/
     let closeBtn = document.querySelector("#close-card");
     let c = document.querySelector("#add-product");
     closeBtn.addEventListener('click', function () {
         if (!c.classList.contains("display-none")) {
             c.classList.add("display-none");
-            }
-        
+        }
+        scrollSmooth("#budget-tree");
     });
+
+/*-----Function for scrolling. Takes div id as parameter------*/
+    function scrollSmooth(hash){
+        
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 500, function () {
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+        $(window).scroll(function () {
+            $(".slideanim").each(function () {
+                let pos = $(this).offset().top;
+
+                let winTop = $(window).scrollTop();
+                if (pos < winTop + 600) {
+                    $(this).addClass("slide");
+                }
+            });
+
+        });
+    }
 
 
 });
